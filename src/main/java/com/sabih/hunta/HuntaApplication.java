d4ce7d200accbefa49d6edb10f9e1539b4721193
padhyar <sabihsaleh@yahoo.com>
2021-12-06 15:22:36 +0000

    Initial Setup

---
+++ src/main/java/com/sabih/hunta/HuntaApplication.java
@@ -0,0 +1,13 @@
+package com.sabih.hunta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntaApplication.class, args);
+	}
+
+}
